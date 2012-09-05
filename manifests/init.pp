6742ecf3b96b605a19009597b663d0c3cef6d5e2
Jurgen Lust <jurgen@beeworks.be>
2012-09-05 16:12:49 +0200

    upgrade to Stask 1.2.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,7 +25,7 @@ class stash (
 	$database_user = "stash",
 	$database_pass = "stash",
 	$number = 4,
-	$version = "1.1.1",
+	$version = "1.2.2",
 	$contextroot = "stash",
 	$webapp_base = "/srv"
 ){
